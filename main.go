3d35019c65ecb3a08b4fdf1677d4db81e952fe90
Yusuf Yildiz <41903311+grealyve@users.noreply.github.com>
2023-08-22 11:56:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,3 @@ func main() {
 
 	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
-
-//psql -U grealyve -d stockapi
-//CREATE DATABASE stockapi;
-//CREATE TABLE stocks(stockid SERIAL PRIMARY KEY, name TEXT, price INT, company TEXT);
